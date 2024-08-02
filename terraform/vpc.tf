668195ff02b5062f0fb49cb75719af14583ca13f
Martin Kulich <martin.kulich@tatum.io>
2024-08-02 15:58:40 +0200

    Fix AZS slice

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
   # TODO: Put this into variable, together with private and public subnets
   cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.region_zones.names, 0, 2)
+  azs             = slice(data.aws_availability_zones.region_zones.names, 0, 3)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
